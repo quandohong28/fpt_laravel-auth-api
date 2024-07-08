6e9bd98ecd918469e8405762dd8c648238d942a7
quandohong28 <quandohong28@gmail.com>
2024-07-08 12:19:15 +0700

    basic auth api token and middleware

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validate = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validate) {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+            return response()->json(['message' => 'User registered successfully!', 'user' => $user], 201);
+        }
+        else {
+            return response()->json(['message' => 'User registered failed!'], 400);
+        }
+
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $request->user()->createToken('auth_token')->plainTextToken;
+        return response()->json(['token' => $token, 'user' => $user]);
+    }
+
+    public function logout(Request $request)
+    {
+        // $request->user()->tokens()->delete();
+
+        // return response()->json(['message' => 'Logged out successfully']);
+
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+}
